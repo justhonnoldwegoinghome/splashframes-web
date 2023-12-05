fa961ff7f2a9ca9b523bfe55c1568089779571d2
justhonnoldwegoinghome <wesleytan19@gmail.com>
2023-12-05 17:51:31 +0800

    (feat) add infinite scrolling

--- src/features/images/api/getImages.ts
+++ src/features/images/api/getImages.ts
@@ -1,4 +1,5 @@
-import useSWR from "swr";
+import useSWRInfinite from "swr/infinite";
+import { useEffect, useState } from "react";
 
 import { get } from "@/utils/apiClient";
 import { APIList } from "@/types/api";
@@ -17,14 +18,27 @@ export function getImages({
   });
 }
 
-export function useImages({
-  page_token,
-  max_page_size,
-}: {
-  page_token?: string | null;
-  max_page_size?: number;
-}) {
-  return useSWR(["/images", page_token], () =>
-    getImages({ page_token, max_page_size })
-  );
+export function useImagesInfinite() {
+  const { data, size, setSize } = useSWRInfinite(getKey, (k) => getImages(k));
+  const [hasEnded, setHasEnded] = useState(false);
+
+  useEffect(() => {
+    if (data && data[data.length - 1].next_page_token === null) {
+      setHasEnded(true);
+    }
+  }, [data]);
+
+  return {
+    data,
+    hasEnded,
+    loadMore: () => setSize(size + 1),
+  };
+}
+
+function getKey(_: any, previousPageData?: APIList<Image>) {
+  return {
+    resource: "images",
+    page_token: previousPageData?.next_page_token,
+    max_page_size: 24,
+  };
 }
--- src/features/images/components/ImagesPage.tsx
+++ src/features/images/components/ImagesPage.tsx
@@ -1,39 +1,50 @@
+import _ from "lodash";
+import clsx from "clsx";
+
 import {
   AddToCartButton,
   RemoveFromCartButton,
   useCart,
 } from "@/features/cart";
 
-import { useImages } from "../api/getImages";
+import { useImagesInfinite } from "../api/getImages";
 import { ImageCard } from "./ImageCard";
 import { ImageCardSkeleton } from "./ImageCardSkeleton";
 
-const DEFAULT_NUM_CARDS = 24;
-
 export function ImagesPage() {
-  const imagesQuery = useImages({
-    page_token: null,
-    max_page_size: DEFAULT_NUM_CARDS,
-  });
-
   const cartQuery = useCart();
+  const { data, hasEnded, loadMore } = useImagesInfinite();
+
+  if (!cartQuery.data || !data)
+    return (
+      <div className="grid grid-cols-2 tablet:grid-cols-3 laptop:grid-cols-4 gap-4">
+        {Array.from(Array(24).keys()).map((i) => (
+          <ImageCardSkeleton key={i} />
+        ))}
+      </div>
+    );
 
   return (
-    <div className="grid grid-cols-2 tablet:grid-cols-3 laptop:grid-cols-4 gap-4">
-      {imagesQuery.data && cartQuery.data
-        ? imagesQuery.data.results.map((img) => (
-            <div key={img.id}>
-              <ImageCard image={img} />
-              {cartQuery.data?.results.includes(img.id) ? (
-                <RemoveFromCartButton id={img.id} />
-              ) : (
-                <AddToCartButton id={img.id} />
-              )}
-            </div>
-          ))
-        : Array.from(Array(DEFAULT_NUM_CARDS).keys()).map((i) => (
-            <ImageCardSkeleton key={i} />
-          ))}
+    <div>
+      <div className="grid grid-cols-2 tablet:grid-cols-3 laptop:grid-cols-4 gap-4">
+        {_.flatten(data.map((d) => d.results)).map((img) => (
+          <div key={img.id}>
+            <ImageCard image={img} />
+            {cartQuery.data?.results.includes(img.id) ? (
+              <RemoveFromCartButton id={img.id} />
+            ) : (
+              <AddToCartButton id={img.id} />
+            )}
+          </div>
+        ))}
+      </div>
+      <button
+        disabled={hasEnded}
+        onClick={() => loadMore()}
+        className={clsx({ "text-red-500 cursor-not-allowed": hasEnded })}
+      >
+        {hasEnded ? "End" : "Load more"}
+      </button>
     </div>
   );
 }
