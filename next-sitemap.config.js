d1652285618a1bceb0fc29c0a3b842d76e7b55b9
justhonnoldwegoinghome <wesleytan19@gmail.com>
2023-12-20 15:05:15 +0800

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://shop.splashframes.com",
+  changefreq: "daily",
+  exclude: ["/me/*"],
+  generateRobotsTxt: true,
+};
