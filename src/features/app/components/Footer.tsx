4140139ef83d69755199f24a2e75fea788a6a60a
justhonnoldwegoinghome <wesleytan19@gmail.com>
2023-12-21 15:11:13 +0800

    style

--- src/features/app/components/Footer.tsx
+++ src/features/app/components/Footer.tsx
@@ -5,41 +5,25 @@ export function Footer() {
   return (
     <div className="border-t">
       <div className="max-w-screen-laptop mx-auto px-[4vw] py-24">
-        <div className="flex flex-col items-center justify-between tablet:flex-row-reverse gap-12 flex-wrap text-sm text-gray-500">
+        <div className="flex flex-col items-center justify-between tablet:flex-row-reverse gap-12 flex-wrap text-gray-500">
           <div className="flex gap-12 flex-wrap">
-            <div>
-              <p className="font-medium text-gray-600 mb-3">Support</p>
-              <div className="flex flex-col gap-1">
-                <Link
-                  href="/privacy-policy"
-                  className="hover:underline underline-offset-4"
-                >
-                  Privacy policy
-                </Link>
-                <Link
-                  href="/refund-policy"
-                  className="hover:underline underline-offset-4"
-                >
-                  Refund policy
-                </Link>
+            {footerItems.map(({ label, links }) => (
+              <div key={label}>
+                <p className="font-medium text-gray-600 mb-3">{label}</p>
+                <div className="flex flex-col gap-1">
+                  {links.map(({ label, href }, i) => (
+                    <Link
+                      key={i}
+                      href={href}
+                      target="_blank"
+                      className="hover:text-sauce"
+                    >
+                      {label}
+                    </Link>
+                  ))}
+                </div>
               </div>
-            </div>
-            <div>
-              <p className="font-medium text-gray-600 mb-3">Contact Us</p>
-              <p>hello@splashframes.com</p>
-            </div>
-            <div className="flex flex-col">
-              <p className="font-medium text-gray-600 mb-3">Find us</p>
-              <div>
-                <Link
-                  href="https://www.instagram.com/splashframes/"
-                  target="_blank"
-                  className="hover:underline underline-offset-4"
-                >
-                  <PiInstagramLogoFill className="text-2xl" />
-                </Link>
-              </div>
-            </div>
+            ))}
           </div>
           <div>
             <p>© 2023, Splashframes</p>
@@ -49,3 +33,37 @@ export function Footer() {
     </div>
   );
 }
+
+const footerItems = [
+  {
+    label: "Customer care",
+    links: [
+      {
+        label: "Privacy policy",
+        href: "/privacy-policy",
+      },
+      {
+        label: "Refund policy",
+        href: "/refund-policy",
+      },
+    ],
+  },
+  {
+    label: "Contact us",
+    links: [
+      {
+        label: "hello@splashframes.com",
+        href: "mailto:hello@splashframes.com",
+      },
+    ],
+  },
+  {
+    label: "Find us",
+    links: [
+      {
+        label: <PiInstagramLogoFill className="text-3xl" />,
+        href: "https://www.instagram.com/splashframes/",
+      },
+    ],
+  },
+];
